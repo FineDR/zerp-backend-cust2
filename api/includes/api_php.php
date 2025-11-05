2081162674ba8dbbec29080dc7891f42420e9682
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-11-05 08:04:05 +0300

    api_login

--- api/includes/api_php.php
+++ api/includes/api_php.php
@@ -11,17 +11,15 @@
  */
 
 // FOLLOWING IS ALWAYS REQUIRED
-echo 'That is it!';
-exit;
+
 if (isset($_SESSION['DatabaseName'])) {
 	$api_DatabaseName = $_SESSION['DatabaseName'];
-	$PathPrefix = __DIR__ . '/../../';
 }else{
 $api_DatabaseName = "zalongwa_weberp";
-$PathPrefix = __DIR__ . '/../';
 }
 
 $AllowAnyone = true;
+$PathPrefix = __DIR__ . '/../../';
 if (!isset($WebErpSessionType)) {
 	include(__DIR__ . '/api_session.php');
 } else {
