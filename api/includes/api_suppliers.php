2261846dedfb66a3cba76f4c80ca966de36eac9a
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-11-15 23:31:55 +0300

    updated supplierInvoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -502,10 +502,6 @@ function InsertSupplierInvoice($Header, $LineDetails, $user, $password) {
 	if (gettype($db)=='integer') {
 		$Errors[0]=NoAuthorisation;
 		return $Errors;
-	}else{
-		$Errors[0]=ZALONGWA;
-		return $Errors;
-
 	}
 
 
