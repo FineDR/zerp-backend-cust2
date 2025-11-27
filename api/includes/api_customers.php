3aa567fc45861028eaba66ca48587526bb861cdd
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-11-27 07:41:58 +0300

    Updated InsertCustomer API

--- api/includes/api_customers.php
+++ api/includes/api_customers.php
@@ -373,10 +373,12 @@ function InsertCustomers($CustomerDetails, $user, $password) {
 	  'VALUES ('.mb_substr($FieldValues,0,-2).') ';
 
 	if (sizeof($Errors)==0) {
-		//$Result = DB_query($SQL);
+		return $SQL;
+		//return $SQL.'idadi ya makosa ni: '.sizeof($Errors).' querry status '.$Result;
+
 		$Result = DB_query($SQL);
 		//return $SQL.'idadi ya makosa ni: '.sizeof($Errors).' querry status '.$Result;
-		return $Result;
+		//return $Result;
 		if (DB_error_no() != 0) {
 			$Errors[0] = DatabaseUpdateFailed;
 		} else {
