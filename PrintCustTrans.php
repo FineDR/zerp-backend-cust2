1a820ad13e89286baeed6ad4b539f3ac1e0fb83c
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-01 17:36:19 +0300

    Updated PrintCustTrans.php

--- PrintCustTrans.php
+++ PrintCustTrans.php
@@ -673,17 +673,8 @@ if (isset($_GET['View']) and $_GET['View'] == 'Yes') {
 		$Result = DB_query($SQL);
 		$MyRow = DB_fetch_row($Result);
 
-		echo '<div class="page_help_text"><b>' . __('The last invoice created was number') . ' ' . $MyRow[0] . '</b><br />' . __('If only a single invoice is required') . ', ' . __('enter the invoice number') . ' ' . __('as both the start and end numbers') . '.</div>';
-echo 'line 677 '.$MyRow[0] ;
-echo '<div class="page_help_text"><b>' 
-    . __('The last invoice created was number') . ' ' . $MyRow[0] 
-    . '</b><br>'
-    . __('If only a single invoice is required') . ', '
-    . __('enter the invoice number') . ' '
-    . __('as both the start and end numbers') 
-    . '.</div>';
-
-exit;
+	    //	echo '<div class="page_help_text"><b>' . __('The last invoice created was number') . ' ' . $MyRow[0] . '</b><br />' . __('If only a single invoice is required') . ', ' . __('enter the invoice number') . ' ' . __('as both the start and end numbers') . '.</div>';
+        echo 'The last invoice created was number'.$MyRow[0] ;
 		$SQL = "SELECT typeno FROM systypes WHERE typeid=11";
 
 		$Result = DB_query($SQL);
