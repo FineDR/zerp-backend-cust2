f7a5e0d695e07a5eb3421d02e02e1095ce340927
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-05 10:33:08 +0300

    updated invoice header

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -124,36 +124,6 @@ function VerifyFactorCompany($factorco , $i, $Errors) {
 	return $Errors;
 }
 
-	function GetTaxes() {
-		/*Gets the Taxes and rates applicable to the tax group of the supplier
-		and SESSION['DefaultTaxCategory'] and the taxprovince of the location that the user is setup to use*/
-
-		$SQL = "SELECT taxgrouptaxes.calculationorder,
-					taxauthorities.description,
-					taxgrouptaxes.taxauthid,
-					taxauthorities.purchtaxglaccount,
-					taxgrouptaxes.taxontax,
-					taxauthrates.taxrate
-			FROM taxauthrates INNER JOIN taxgrouptaxes ON
-				taxauthrates.taxauthority=taxgrouptaxes.taxauthid
-				INNER JOIN taxauthorities ON
-				taxauthrates.taxauthority=taxauthorities.taxid
-			WHERE taxgrouptaxes.taxgroupid=" . $this->TaxGroup . "
-			AND taxauthrates.dispatchtaxprovince=" . $this->LocalTaxProvince . "
-			AND taxauthrates.taxcatid = " . $_SESSION['DefaultTaxCategory'] . "
-			ORDER BY taxgrouptaxes.calculationorder";
-		$GetTaxRatesResult = DB_query($SQL);
-
-		while ($MyRow = DB_fetch_array($GetTaxRatesResult)){
-
-			$this->Taxes[$MyRow['calculationorder']] = new Tax($MyRow['calculationorder'],
-																$MyRow['taxauthid'],
-																$MyRow['description'],
-																$MyRow['taxrate'],
-																$MyRow['taxontax'],
-																$MyRow['purchtaxglaccount']);
-		}
-	}
 
 /* Common SQL Functions */
 /*
@@ -1596,6 +1566,37 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 
 	$LocalTaxProvinceRow = DB_fetch_row($LocalTaxProvinceResult);
 	$LocalTaxProvince = $LocalTaxProvinceRow[0];
+
+	function GetTaxes() {
+		/*Gets the Taxes and rates applicable to the tax group of the supplier
+		and SESSION['DefaultTaxCategory'] and the taxprovince of the location that the user is setup to use*/
+
+		$SQL = "SELECT taxgrouptaxes.calculationorder,
+					taxauthorities.description,
+					taxgrouptaxes.taxauthid,
+					taxauthorities.purchtaxglaccount,
+					taxgrouptaxes.taxontax,
+					taxauthrates.taxrate
+			FROM taxauthrates INNER JOIN taxgrouptaxes ON
+				taxauthrates.taxauthority=taxgrouptaxes.taxauthid
+				INNER JOIN taxauthorities ON
+				taxauthrates.taxauthority=taxauthorities.taxid
+			WHERE taxgrouptaxes.taxgroupid=" . $this->TaxGroup . "
+			AND taxauthrates.dispatchtaxprovince=" . $this->LocalTaxProvince . "
+			AND taxauthrates.taxcatid = " . $_SESSION['DefaultTaxCategory'] . "
+			ORDER BY taxgrouptaxes.calculationorder";
+		$GetTaxRatesResult = DB_query($SQL);
+
+		while ($MyRow = DB_fetch_array($GetTaxRatesResult)){
+
+			$this->Taxes[$MyRow['calculationorder']] = new Tax($MyRow['calculationorder'],
+																$MyRow['taxauthid'],
+																$MyRow['description'],
+																$MyRow['taxrate'],
+																$MyRow['taxontax'],
+																$MyRow['purchtaxglaccount']);
+		}
+	}
 	GetTaxes();
 return 'line 1569: ';
 	/*
