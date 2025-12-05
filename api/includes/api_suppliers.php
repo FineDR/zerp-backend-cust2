f2e59daf3c474425441c01a21359c59d30e90479
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-05 05:34:10 +0300

    updated invoice header

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -1574,7 +1574,6 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 		$_SESSION['SuppTrans']->InvoiceOrCredit = 'Invoice';
     */
 	$InvoiceOrCredit = 'Invoice';
-   return 'LocalTax Province = '.$LocalTaxProvince;
 
    //mpaka hapa tuna kosa vifuatavyo
    //invoice totals
