04628263ec006bbf6bf92cb342f8fdf29811364b
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-09 22:52:04 +0300

    updated API for SupplierInvoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -618,7 +618,8 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 	
 	//get user default location
 	//tempo soln: hardcode user
-	$SQL = "SELECT defaultlocation FROM www_users WHERE userid = '$user'";
+	//$SQL = "SELECT defaultlocation FROM www_users WHERE userid = 'admin'";
+	$SQL = "SELECT defaultlocation FROM www_users WHERE userid = '".$user."'";
 
 	return $SQL;
 
