eba4725fd67d8a5c203e6fb94261ab2fe9d86816
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-14 08:03:04 +0300

    updated API for salesorders

--- api/includes/api_salesorders.php
+++ api/includes/api_salesorders.php
@@ -852,7 +852,7 @@ function InvoiceSalesOrder($OrderNo, $User, $Password) {
 						INNER JOIN currencies
 						ON debtorsmaster.currcode=currencies.currabrev
 						WHERE salesorders.orderno = '" . $OrderNo . "'";
-
+return $OrderHeaderSQL;
 	$OrderHeaderResult = api_DB_query($OrderHeaderSQL);
 	if (DB_error_no() != 0) {
 		$Errors[] = NoReadOrder;
