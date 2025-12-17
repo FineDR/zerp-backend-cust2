9a7706f0b59ea9f517db0c7b95955dd5b7837521
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-17 09:10:00 +0300

    updated API for Insert Location

--- api/includes/api_locations.php
+++ api/includes/api_locations.php
@@ -150,7 +150,7 @@ function InsertLocation($Location, $user, $password) {
 		$FieldValues.='"'.$Value.'", ';
 	}
 	if (sizeof($Errors)==0) {
-		$SQL = "INSERT INTO locations (" . mb_substr($FieldNames,0,-2) . ")
+		$SQL = "INSERT INTO locations ('" . mb_substr($FieldNames,0,-2) . "')
 					VALUES ('" . mb_substr($FieldValues,0,-2) . "') ";
 		$Result = DB_query($SQL);
 		if (DB_error_no() != 0) {
