666b557300795b61890b59f08666c8f338f47aa4
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-18 13:12:09 +0300

    updated API for Insert Sales Invoice

--- api/includes/api_debtortransactions.php
+++ api/includes/api_debtortransactions.php
@@ -1259,7 +1259,7 @@ function InsertSalesInvoice($InvoiceDetails, $user, $password) {
 	$Errors=VerifyTransNO($InvoiceDetails['transno'], 10, sizeof($Errors), $Errors);
 	//return $InvoiceDetails['trandate'];
 	//$Errors=VerifyDateFormat($InvoiceDetails['trandate'], sizeof($Errors), $Errors);
-	$Errors=VerifyTransactionDate($InvoiceDetails['trandate'], sizeof($Errors), $Errors);
+	$Errors=VerifyInvoiceTransactionDate($InvoiceDetails['trandate'], sizeof($Errors), $Errors);
 	if (isset($InvoiceDetails['settled'])){
 		$Errors=VerifySettled($InvoiceDetails['settled'], sizeof($Errors), $Errors);
 	}
