8bbbdaea54c8a71faa40368a2bde4381cbbfb98e
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-18 13:40:50 +0300

    updated API for Insert Sales Invoice

--- api/includes/api_debtortransactions.php
+++ api/includes/api_debtortransactions.php
@@ -1324,7 +1324,6 @@ function InsertSalesInvoice($InvoiceDetails, $user, $password) {
 		$SQL = "INSERT INTO debtortrans (" . mb_substr($FieldNames,0,-2) .")
 								VALUES (" . mb_substr($FieldValues,0,-2) .") ";
 		$Result = DB_query($SQL);
-																					return $Result.' --> '.$SQL;
 		$SQL = "UPDATE systypes SET typeno='" . GetNextTransNo(10) . "' WHERE typeid=10";
 		$Result = DB_query($SQL);
 		$SalesGLCode=GetSalesGLCode($SalesArea, $PartCode);
