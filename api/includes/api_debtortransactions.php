d0c0b5476a05debcc4b500d583dd87ad88ea211c
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-18 12:50:23 +0300

    updated API for Insert Sales Invoice

--- api/includes/api_debtortransactions.php
+++ api/includes/api_debtortransactions.php
@@ -1313,7 +1313,6 @@ function InsertSalesInvoice($InvoiceDetails, $user, $password) {
 		$FieldNames.=$key.', ';
 		$FieldValues.='"'.$Value.'", ';
 	}
-	return $PartCode;
 	if (sizeof($Errors)==0) {
 		DB_Txn_Begin();
 		$SQL = "INSERT INTO debtortrans (" . mb_substr($FieldNames,0,-2) .")
@@ -1335,6 +1334,7 @@ function InsertSalesInvoice($InvoiceDetails, $user, $password) {
 										0,
 										'" . $InvoiceDetails['jobref'] . "',
 										1)";
+											return $PartCode.' --> '.$SQL;
 		$Result = api_DB_query($SQL);
 		$SQL="INSERT INTO gltrans VALUES(null,
 										10,
