5ced39b7f6eacf0989d9295a2447583587746cf7
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-28 07:11:06 +0300

    updated InsertSupplierInvoiceHeader

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -629,7 +629,6 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 		$Errors[0] = NoSupplierExist;
 		return $Errors;
 	}
-		return $SQL;
 	$suppname = $MyRow['suppname'];
 	$terms = $MyRow['terms'];
 	$currcode = $MyRow['currcode'];
@@ -639,6 +638,8 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 	$taxgroupdescription = $MyRow['taxgroupdescription'];
 	$InvoiceNo = $SupplierHeader['suppreference'];
 
+	return $suppname;
+	
 	if ($MyRow['daysbeforedue'] == 0) {
 		$terms = '1' . $MyRow['dayinfollowingmonth'];
 	}
