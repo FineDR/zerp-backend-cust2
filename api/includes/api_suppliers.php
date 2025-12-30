c0ffd40c77fa643cf239a98d006cfb10bfb27644
Juma Lungo <juma.lungo@zalongwa.co.tz>
2025-12-30 21:26:00 +0300

    updated API for supplier

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -1027,7 +1027,7 @@ function InsertSupplierInvoiceHeader($SupplierHeader, $SupplierInvoiceLine, $use
 													'" . mb_substr('PO: ' . $OrderNo . ' ' . $SupplierID . ' - ' . $StockID . ' - ' . DB_escape_string($ItemDescription) . ' x ' . $ReceiveQty . ' @ ' . locale_number_format($decimalplaces), 0, 200) . "',
 													'" . $CurrentStandardCost * $ReceiveQty . "'
 													)";
-return 'line 1030: '.$SQL;
+
 							//$ErrMsg = __('CRITICAL ERROR') . '! ' . __('NOTE DOWN THIS ERROR AND SEEK ASSISTANCE') . ': ' . __('The purchase GL posting could not be inserted because');
 							//$Result = DB_query($SQL, $ErrMsg, '', true);
 							$Result = api_DB_query($SQL);
@@ -1110,7 +1110,7 @@ return 'line 1030: '.$SQL;
 			//} //only allow auto receiving of all lines if the PO is authorised
 
 		//} //only allow auto receiving if the user has permission to receive goods
-
+return 'line 1113: '.$SQL;
 	} // Page called with link to receive all the items on a PO
 
 	//=========mwisho wa PO ====================//
