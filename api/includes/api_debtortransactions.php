d841b85222f2119e24d4563376aba7a63beb8d99
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-01-15 18:30:02 +0300

    updated API for GetAgedDebtors

--- api/includes/api_debtortransactions.php
+++ api/includes/api_debtortransactions.php
@@ -2117,7 +2117,7 @@ function GetAgedDebtors($ReportCriteria, $CustomerCriteria, $user, $password) {
 		$Answer[$i]['TotCurr'] += ($AgedAnalysis['balance']-$AgedAnalysis['due']); $i++;
 		$Answer[$i]['TotOD1'] += ($AgedAnalysis['overdue1']-$AgedAnalysis['overdue2']); $i++;
 		$Answer[$i]['TotOD2'] += $AgedAnalysis['overdue2']; $i++;
-	return 'line 2120: '.$Answer[$i]['CurrDecimalPlaces'];
+	return 'line 2120: '.$i;
 
 		if ($_POST['DetailedReport']=='Yes') {
 
