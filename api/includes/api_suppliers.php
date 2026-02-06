5bdcb03704a7e54cfeece4f88ff2667bbcab520e
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-02-06 05:17:25 +0300

    updted supplier invoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -631,13 +631,18 @@ function InsertSupplierInvoice($SupplierInvoiceHeader, $SupplierInvoiceLine, $us
 		}
 	}
 
-	$Errors=VerifySupplierNoExists($SupplierInvoiceHeader['supplierno'], sizeof($Errors), $Errors);
-	if (isset($SupplierInvoiceHeader['trandate'])){
-		$Errors=VerifyDateFormat($SupplierInvoiceHeader['trandate'], sizeof($Errors), $Errors);
+	$Errors = VerifySupplierNoExists(
+		$SupplierInvoiceHeader['supplierno'],
+		$Errors
+	);
+
+	if (isset($SupplierInvoiceHeader['trandate'])) {
+		$Errors = VerifyDateFormat(
+			$SupplierInvoiceHeader['trandate'],
+			$Errors
+		);
 	}
-	if (isset($SupplierInvoiceHeader['deliverydate'])){
-		$Errors=VerifyDateFormat($SupplierInvoiceHeader['deliverydate'], sizeof($Errors), $Errors);
-	}
+
 	return $SupplierInvoiceHeader['supplierno']; exit;
 	/*
 	CREATE TABLE IF NOT EXISTS api_supplier_invoice_drafts (
