f4e3e532e0b1e31a369c28e89181440703ae3d80
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-02-16 23:02:10 +0300

    update supplier invoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -636,7 +636,9 @@ function InsertSupplierInvoice($SupplierInvoiceHeader, $SupplierInvoiceLine, $us
 	/** Verify that the supplier number is valid, and already exists. */
 	//$supplierNo = '104824986';
 	$supplierNo = $SupplierInvoiceHeader['supplierno'];
-	if ((strlen($supplierNo)<1) or (strlen($supplierNo)>20)) {
+			return mb_strlen($supplierNo); exit;
+
+	if ((mb_strlen($supplierNo)<1) or (mb_strlen($supplierNo)>20)) {
 		$Errors[$i] = IncorrectDebtorNumberLength;
 	}
 		return $supplierNo; exit;
