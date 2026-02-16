60ab3cc37570e58ddd9ead7a335cd4fc901885eb
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-02-16 21:47:06 +0300

    update supplier invoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -742,15 +742,21 @@ function InsertSupplierInvoice($SupplierInvoiceHeader, $SupplierInvoiceLine, $us
 	 * Stateless Supplier Invoice XML-RPC API for webERP
 	 * Auth: cookie session (must be logged-in); API stores drafts in MySQL.
 	 */
-$path = '../../includes/DefineSuppTransClass.php';
-return $path; exit;
-	//require_once(__DIR__ . '/../includes/session.php');
-	require_once(__DIR__ . '/../includes/DefineSuppTransClass.php');
 	/*
+    $path = '../../includes/DefineSuppTransClass.php';
+    return $path; exit;
+	require_once(__DIR__ . '/../includes/session.php');
+	require_once(__DIR__ . '/../includes/DefineSuppTransClass.php');
 	require_once(__DIR__ . '../../includes/SQL_CommonFunctions.php');
 	require_once(__DIR__ . '../../includes/StockFunctions.php');
 	require_once(__DIR__ . '../../includes/GLFunctions.php');
 */
+
+	require_once '../../includes/session.php';
+	require_once '../../includes/DefineSuppTransClass.php';
+	require_once '../../includes/SQL_CommonFunctions.php';
+	require_once '../../includes/StockFunctions.php';
+	require_once '../../includes/GLFunctions.php';
 
 	return $supplierNo; exit;
 	if (!isset($_SESSION['UserID']) || $_SESSION['UserID'] === '') {
