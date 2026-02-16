656e4fa113cd112924b9da22df7170d1a1901792
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-02-16 21:40:21 +0300

    update supplier invoice

--- api/includes/api_suppliers.php
+++ api/includes/api_suppliers.php
@@ -742,9 +742,10 @@ function InsertSupplierInvoice($SupplierInvoiceHeader, $SupplierInvoiceLine, $us
 	 * Stateless Supplier Invoice XML-RPC API for webERP
 	 * Auth: cookie session (must be logged-in); API stores drafts in MySQL.
 	 */
-
+$path = __DIR__ . '/../includes/DefineSuppTransClass.php';
+return $path; exit;
 	//require_once(__DIR__ . '/../includes/session.php');
-	require_once(__DIR__ . '../../includes/DefineSuppTransClass.php');
+	require_once(__DIR__ . '/../includes/DefineSuppTransClass.php');
 	/*
 	require_once(__DIR__ . '../../includes/SQL_CommonFunctions.php');
 	require_once(__DIR__ . '../../includes/StockFunctions.php');
