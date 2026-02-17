86b589e8c1ecbd6928b29af1bacf581fd22a022c
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-02-17 06:21:34 +0300

    update supplier invoice

--- api/includes/api_salesorders.php
+++ api/includes/api_salesorders.php
@@ -524,7 +524,7 @@ function InsertSalesOrderLine($OrderLine, $user, $password) {
 			$Errors[0]=0;
 		}
 	}
-	return $Errors;
+	return $Errors.' '.$SQL;
 }
 
 /** Modify a customer sales order line in webERP. The order header must
