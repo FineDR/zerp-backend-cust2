c2a465da4ca1610786892411128b32c87c0b5c5d
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-03-25 07:44:18 +0300

    updated setstockprice

--- api/includes/api_stock.php
+++ api/includes/api_stock.php
@@ -699,16 +699,7 @@ function SetStockPrice($StockID, $Currency, $SalesType, $Price, $user, $password
 										'". $Price ."',
 										'')";
         */
-				$SQL = "INSERT INTO prices (stockid,
-								typeabbrev,
-								currabrev,
-								debtorno,
-								price,
-								branchcode,
-								startdate,
-								enddate)
-							VALUES ('".$StockID."','".$SalesType."','".$Currency."','','" . $Price. "','','" . FormatDateForSQL($StartDate). "','" . FormatDateForSQL($EndDate) . "'
-							)";
+				$SQL = "INSERT INTO prices (stockid,typeabbrev,currabrev,debtorno,price,branchcode,startdate,enddate) VALUES ('".$StockID."','".$SalesType."','".$Currency."','','" . $Price. "','','" . FormatDateForSQL($StartDate). "','" . FormatDateForSQL($EndDate) . "')";
 	} else {
 		$SQL='UPDATE prices SET price='. $Price .
 				" WHERE  stockid='" .$StockID."'
