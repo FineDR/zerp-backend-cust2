f58b013e4b5c65d5cbc019b2268687fc1f4f91bf
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-04-18 09:12:02 +0300

    updated Trial Balance

--- includes/SetDomPDFOptions.php
+++ includes/SetDomPDFOptions.php
@@ -2,17 +2,30 @@
 
 /* set all options for DomPDF in one place for reusability and consistency */
 
-require 'vendor/autoload.php';
+$ProjectRoot = dirname(__DIR__);
+$Autoload = $ProjectRoot . '/vendor/autoload.php';
 
-use Dompdf\Options;
+if (!class_exists('Dompdf\\Options') and file_exists($Autoload)) {
+	require_once($Autoload);
+}
 
-$DomPDFOptions = new Options();
+if (!class_exists('Dompdf\\Options')) {
+	$Message = 'Dompdf is not available. Run composer install and check that vendor/autoload.php can be loaded.';
+	if (function_exists('prnMsg')) {
+		prnMsg($Message, 'error');
+	} else {
+		echo $Message;
+	}
+	exit();
+}
+
+$DomPDFOptions = new \Dompdf\Options();
 
 $DomPDFOptions->set('isHtml5ParserEnabled', true);
 $DomPDFOptions->set('isRemoteEnabled', true);
 
 if (isset($SymlinkImageDir) and ($SymlinkImageDir != '')) {
-	$DomPDFOptions->setChroot([$PathPrefix, $SymlinkImageDir]);
+	$DomPDFOptions->setChroot([$PathPrefix ?? $ProjectRoot, $SymlinkImageDir]);
 } else {
-	$DomPDFOptions->setChroot([$PathPrefix]);
+	$DomPDFOptions->setChroot([$PathPrefix ?? $ProjectRoot]);
 }
