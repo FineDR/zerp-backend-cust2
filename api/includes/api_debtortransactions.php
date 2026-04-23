1556b1f011ee1f40bb70a498f7bc59a479665dbd
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-04-23 18:53:35 +0300

    updated api

--- api/includes/api_debtortransactions.php
+++ api/includes/api_debtortransactions.php
@@ -525,7 +525,7 @@ function InsertDebtorReceipt($Receipt, $User, $Password) {
 						'" . -$Receipt['amountfx'] . "',
 						'" . -$Receipt['discountfx'] . "',
 						'" . $Receipt['paymentmethod'] . "')";
-
+return $SQL;
 	$Result = api_DB_query($SQL,'', '', true);
 
 	$SQL = "UPDATE debtorsmaster SET lastpaiddate = '" . $Receipt['trandate'] . "',
