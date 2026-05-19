bcd1606893b176983ce815280bbc19244363d8fc
FineDr <finesawa36@gmail.com>
2026-05-19 13:16:38 +0300

    remove scripts and temp files

--- scratch/check_currencies.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('includes/session.php');
-echo "ExchangeRateFeed: " . $_SESSION['ExchangeRateFeed'] . "\n";
-echo "UpdateCurrencyRatesDaily: " . $_SESSION['UpdateCurrencyRatesDaily'] . "\n";
-$res = DB_query("SELECT currabrev, rate FROM currencies");
-while ($row = DB_fetch_assoc($res)) {
-    echo "Currency: " . $row['currabrev'] . " Rate: " . $row['rate'] . "\n";
-}
-?>
--- scratch/update_pass.sql
+++
@@ -1,1 +0,0 @@
-UPDATE www_users SET password = '$2y$12$tSrb.AKEiYrwbDgzFuRS7uO.bDKwBqPFU.QAvcWi.bBUDc4KJ1vl.' WHERE userid = 'admin';
